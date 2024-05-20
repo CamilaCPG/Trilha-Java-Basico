7fa9355ab00b760ae9697ba9250ed1a0d6587e0d
CamilaCPG <camila.cpguedes@gmail.com>
2024-05-20 16:08:24 -0300

    Exercício pedido feito! :]

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá!! Por gentileza, insira as seguintes informações:");
+
+        System.out.println("1. Número da conta:");
+        int contaNumero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("2. Agência:");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("3. Seu nome");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("4. Saldo em conta:");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + contaNumero + " e seu saldo " + saldo + " já está disponível para saque!!");
     }
 }
